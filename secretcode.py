051599c4a382382437934ce2f8202be964ed932e
Ruben Rodriguez Loya Jr <49329360+loyaruben3@users.noreply.github.com>
2022-05-19 02:35:42 -0700

    Delete secretcode.py

--- secretcode.py
+++
@@ -1,4 +0,0 @@
-# class token():
-#     def token():
-secret_code = 'NzAyMzg1ODg0MjI1NDcwNTQ2.XumpDg.bc9y_-fkYsV3Suz6Cn0zDvImMc8'
-secretCodeV2 = 'NzMxNzI5NzkyNTcxMDE1MTcw.XwqSeQ.AC79dQvA5D3fdxrroIXIdC_WN84'
